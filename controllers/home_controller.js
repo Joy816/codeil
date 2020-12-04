8299b3ab92283d71e63a323ffe62e3131a0d537d
Joy <jsngpt816@gmail.com>
2020-12-04 23:40:51 +0530

    create home.ejs and render home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,13 @@
 module.exports.home = function (req , res){
 
-    return res.end("<h1> This is a home page!</h1>");
+    // return res.end("<h1> This is a home page!</h1>");
+
+    return res.render ('home',
+    {
+        title: "Home"
+
+    }
+    );
 }
 
 
