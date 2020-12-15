daa8074e5e2c75148d37b0ec1a3479e828af67b6
Joy <jsngpt816@gmail.com>
2020-12-15 08:37:59 +0530

    start scss

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ const cookieParser = require ('cookie-parser');
 const expressLayouts = require ('express-ejs-layouts');
 const app = express();
 const port = 8000;
-
 //importing mongoDB 
 const db = require ("./config/mongoose");
-
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-stratergy');
-// const { MongoStore } = require('connect-mongo');
 const MongoStore = require ('connect-mongo')(session);
+// requiring sass middleware
+const saasMiddleware = require ('node-sass-middleware');
+
+app.use(saasMiddleware(
+    {
+        src:'./assets/scss',
+        dest: './assets/css',
+        debug : 'true',
+        outputStyle: 'extended',
+        prefix : '/css'
+
+
+    }
+));
 
 //using Express layouts
 app.use(expressLayouts);
